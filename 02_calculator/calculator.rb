c05bf46cc2e5a18d30272515a2d476df9fe7d279
kim <matt.kim@utexas.edu>
2018-01-27 21:06:24 -0600

    Completed exercise 03 and bonus questions

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+
+def add(a, b)
+    return a + b
+end
+
+def subtract(a, b)
+    return a - b
+end
+
+def sum(array)
+    sum = 0
+    array.each do |num|
+        sum += num
+    end
+    return sum
+end
+
+def multiply(a, *b)
+    b.each do |n|
+        a *= n
+    end
+    return a
+end
+
+def power(a, b)
+    return a ** b 
+end
+
+def factorial(a)
+    value = 1
+    while a > 1
+        value *= a
+        a -= 1
+    end
+    return value
+end
